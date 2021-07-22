195fde40cb04cbf417497aeb9599e7a057fecc22
William <68653853+will-i-amv@users.noreply.github.com>
2021-07-22 14:46:31 -0400

    Modify module docstring

--- Chapter 3/wargame_distribution/wargame/attackoftheorcs_v1_1.py
+++ Chapter 3/wargame_distribution/wargame/attackoftheorcs_v1_1.py
@@ -1,53 +1,25 @@
-from __future__ import print_function
-"""ch01_ex03_AbstractBaseClass
+"""wargame.attackoftheorcs
 
-A text-based game to acquire a hut by defeating the enemy (OOP- using  ABC).
+This module contains the AttackOfTheOrcs class implementation.
+
+This is the main class with the high level logic to play Attack of the Orcs
+game.
 
 This module is compatible with Python 3.5.x. It contains
 supporting code for the book, Learning Python Application Development,
 Packt Publishing.
 
-Demonstrates use of Abstract Base Class (ABC) in Python. It is a hint
-to an EXERCISE of Chapter 1.
-
-The player inputs a hut number. If the occupant is an enemy, the player is
-given an option to 'attack'. Player wins if he defeats the enemy.
-Additionally,the player can 'run away' from the combat, get healed
-in friendly hut and then resume the fight.
-
-In the aforementioned book this is also referred to as
-"Attack of the Orcs v1.0.0". More details can be found in the relevant
-chapter of the book..
-
-RUNNING THE PROGRAM:
---------------------
-- Python 3.5.x must be installed on your system.
-- It is assumed that you have Python 3.5 available in your environment
-  variable PATH. It will be typically available as 'python' or 'python3'.
-- Here is the command to execute this code from command prompt
-
-        $ python ch01_ex03_AbstractBaseClass.py
-        ( OR $ python3 ch01_ex03_AbstractBaseClass.py)
-
-- See the README file for more information. Or visit python.org for OS
-  specific instructions on executing Python from a command prompt.
-
-.. seealso:: ch01_ex03.py -- Same example without using ABC
-
 .. todo::
 
-1. The code comments and function descriptions in this file are
-   intentionally kept to a minimum! See a later chapter of the book to
+   The code comments and function descriptions in this file are
+   intentionally kept to a minimum! See Chapter 4 of the book to
    learn about the code documentation and best practices!
-   Feel free to add documentation after reading that chapter.
-   Description of the code can be found in the book.
-2. Split the code into smaller modules
-3. See the other TODO comments..things you can try fixing as an exercise!
 
 :copyright: 2016, Ninad Sathaye
 
 :license: The MIT License (MIT) . See LICENSE file for further details.
 """
+from __future__ import print_function
 import random
 import sys
 from gameutils import print_bold
