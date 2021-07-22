316bd533544140932340731ae40d6cb1c780ee16
William <68653853+will-i-amv@users.noreply.github.com>
2021-07-22 14:38:32 -0400

    Move Knight class to knight.py Add the required imports in the new module Modify the main file to import the class from the module

--- Chapter 3/wargame_distribution/wargame/attackoftheorcs_v1_1.py
+++ Chapter 3/wargame_distribution/wargame/attackoftheorcs_v1_1.py
@@ -55,6 +55,7 @@ from abc import ABCMeta, abstractmethod
 from gameuniterror import GameUnitError
 from gameutils import print_bold, weighted_random_selection
 from abstractgameunit import AbstractGameUnit
+from knight import Knight
 from hut import Hut
 
 
@@ -64,70 +65,6 @@ if sys.version_info < (3, 0):
     "Python version: {}.{} ".format(sys.version_info[0], sys.version_info[1]))
     print("Exiting...")
     sys.exit(1)
-
-
-class Knight(AbstractGameUnit):
-    """ Class that represents the game character 'Knight'
-
-    The player instance in the game is a Knight instance. Other Knight
-    instances are considered as 'friends' of the player and is
-    indicated by the attribute `self.unit_type` .
-    """
-    def __init__(self, name='Sir Foo'):
-        super().__init__(name=name)
-        self.max_hp = 40
-        self.health_meter = self.max_hp
-        self.unit_type = 'friend'
-
-    def info(self):
-        """Print basic information about this character"""
-        print("I  am a Knight!")
-
-    def acquire_hut(self, hut):
-        """Fight the combat (command line) to acquire the hut
-
-        .. todo::   acquire_hut method can be refactored.
-                   Example: Can you use self.enemy instead of calling
-                   hut.occupant every time?
-        """
-        print_bold("Entering hut {}...".format(hut.number), end=' ')
-        is_enemy = (isinstance(hut.occupant, AbstractGameUnit) and
-                    hut.occupant.unit_type == 'enemy')
-        continue_attack = 'y'
-
-        if is_enemy:
-            print_bold("Enemy sighted!")
-            self.show_health(bold=True, end=' ')
-            hut.occupant.show_health(bold=True, end=' ')
-
-            while continue_attack:
-                continue_attack = input(".......continue attack? (y/n): ")
-                if continue_attack == 'n':
-                    self.run_away()
-                    break
-                self.attack(hut.occupant)
-                if hut.occupant.health_meter <= 0:
-                    print("")
-                    hut.acquire(self)
-                    break
-                if self.health_meter <= 0:
-                    print("")
-                    break
-        else:
-            if hut.get_occupant_type() == 'unoccupied':
-                print_bold("Hut is unoccupied")
-            else:
-                print_bold("Friend sighted!")
-            hut.acquire(self)
-            self.heal()
-
-    def run_away(self):
-        """Abandon the battle.
-
-        .. seealso:: `self.acquire_hut`
-        """
-        print_bold("RUNNING AWAY...")
-        self.enemy = None
 
 
 class OrcRider(AbstractGameUnit):
