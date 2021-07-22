3508a602ac53dc9bcba8a8892a77c9076aeebe2c
William <68653853+will-i-amv@users.noreply.github.com>
2021-07-22 17:19:34 -0400

    Add upload_private.sh bash script * The script does the same thing as upload.sh, but uses a private PyPi repo with pypiserver * The documentation of pypiserver is at https://github.com/pypiserver/pypiserver

---
+++ Chapter 3/wargame_distribution/upload_private.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#	Create a new private source distribution
+python setup.py sdist
+
+#	Start the local server
+pypi-server -p 8081 ./dist
+
+#	Install your project from the local server
+pip install -i http://localhost:8081 wsebas_attackoftheorcs
