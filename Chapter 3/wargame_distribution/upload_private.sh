b42497d329cd42dd4ef89282a1908efa8179e50a
William <68653853+will-i-amv@users.noreply.github.com>
2021-07-23 16:41:30 -0400

    update upload_private

--- Chapter 3/wargame_distribution/upload_private.sh
+++ Chapter 3/wargame_distribution/upload_private.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-#	Create a new private source distribution
+# Install the package
+pip install pypiserver
+
+# Create a new private source distribution
 python setup.py sdist
 
-#	Start the local server
+# Start the local server
 pypi-server -p 8081 ./dist
 
-#	Install your project from the local server
+# Install your project from the local server
 pip install -i http://localhost:8081 wsebas_attackoftheorcs
